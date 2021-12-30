632e058c893f4af4a53d17f49c37c92f35e432ac
Supertiger <saawalkhn@googlemail.com>
2021-12-30 22:31:11 +0000

    testing out payload sync

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,18 @@ const heroProperties = {
   }
 }
 
+const maps = {
+  first_map: {
+    payloadRoute: [
+      {y: 11, x: 17},
+      {y: 11, x: 40},
+      {y: 20, x: 40},
+    ]
+  }
+}
+
+
+
 interface Player {
   client: Socket;
   x: number;
@@ -20,6 +32,21 @@ interface Player {
 }
 
 const players: {[key: string]: Player} = {};
+
+
+const playerSize = 50;
+const tileSize = 50;
+
+const payloadSpeed = 10;
+const payloadWidth = 150;
+const payloadHeight = 100;
+
+const map = maps.first_map;
+let payloadX = map.payloadRoute[0].x;
+let payloadY = map.payloadRoute[0].y;
+
+let currentPayloadRoute = 0;
+let playersNearPayload = [];
 
 io.on('connection', client => { 
   client.emit("overrideHeroProperties", heroProperties)
@@ -37,6 +64,8 @@ io.on('connection', client => {
     currentY: 400,
     currentAngle: 0
   };
+
+
 
   client.on("playerMove", position => {
     player.currentX = position[0];
@@ -65,38 +94,90 @@ io.on('connection', client => {
 });
 
 
+
+
+let lastTime: null | number = null;
 function gameLoop() {
+  if (!lastTime) {
+    lastTime = performance.now();
+    return;
+  }
+  const delta = performance.now() - lastTime;
+
+  playersNearPayload = [];
   for (let playerId in players) {
     const player = players[playerId];
+    handleMovement(playerId);
+    handlePayloadMovement(playerId, delta);
 
-    const lastX = player.x;
-    const lastY = player.y;
+  }
+  lastTime = performance.now();
+}
 
-    const newX = player.currentX;
-    const newY = player.currentY;
+function handlePayloadMovement(playerId: string, delta: number) {
+  const player = players[playerId];
 
-    const oldAngle = player.angle;
-    const newAngle = player.currentAngle;
-    const didAngleChange = oldAngle !== newAngle;
+  const worldX = payloadX * tileSize - (payloadWidth /2) + (playerSize / 2)
+  const worldY = payloadY * tileSize - (payloadHeight/2) + (playerSize / 2)
+    const xRadius = 150;
+    const yRadius = 120;
+    // check if player is near the payload
+    const xDistance = Math.abs(player.x - worldX);
+    const yDistance = Math.abs(player.y - worldY);
 
-    const deltaX = newX - lastX;
-    const deltaY = newY - lastY;
-
-    if (deltaX !== 0 || deltaY !== 0) {
-      player.x = newX;
-      player.y = newY;
-      if (didAngleChange) {
-        player.angle = newAngle;
-        player.client.broadcast.emit("playerMoveAndRotate", [playerId, deltaX, deltaY, newAngle])
-      } else {
-        player.client.broadcast.emit("playerMove", [playerId, deltaX, deltaY])
+    // this.pushing = false;
+    if (xDistance <= xRadius && yDistance <= yRadius) {
+      const nextRoutePath = map.payloadRoute[currentPayloadRoute + 1]!;
+      const xReached = nextRoutePath.x === Math.floor(payloadX);
+      const yReached = nextRoutePath.y === Math.floor(payloadY);
+      // this.pushing = true;
+      if (xReached && yReached) {
+        currentPayloadRoute++;
+      }
+      if (!xReached) {
+        payloadX += (payloadSpeed / tileSize) * (delta / tileSize);
+        io.emit("payloadMoveX", payloadX);
+        // this.angle = 0;
+      }
+      if (!yReached) {
+        payloadY+= (payloadSpeed / tileSize) * (delta / tileSize);
+        io.emit("payloadMoveY", payloadY);
+        // this.angle = 90;
       }
     }
-    if (deltaX === 0 && deltaY === 0) {
-      if (didAngleChange) {
-        player.angle = newAngle;
-        player.client.broadcast.emit("playerRotate", [playerId, newAngle])
-      }
+
+}
+
+function handleMovement(playerId: string) {
+  const player = players[playerId];
+  
+  const lastX = player.x;
+  const lastY = player.y;
+
+  const newX = player.currentX;
+  const newY = player.currentY;
+
+  const oldAngle = player.angle;
+  const newAngle = player.currentAngle;
+  const didAngleChange = oldAngle !== newAngle;
+
+  const deltaX = newX - lastX;
+  const deltaY = newY - lastY;
+
+  if (deltaX !== 0 || deltaY !== 0) {
+    player.x = newX;
+    player.y = newY;
+    if (didAngleChange) {
+      player.angle = newAngle;
+      player.client.broadcast.volatile.emit("playerMoveAndRotate", [playerId, deltaX, deltaY, newAngle])
+    } else {
+      player.client.broadcast.volatile.emit("playerMove", [playerId, deltaX, deltaY])
+    }
+  }
+  if (deltaX === 0 && deltaY === 0) {
+    if (didAngleChange) {
+      player.angle = newAngle;
+      player.client.broadcast.volatile.emit("playerRotate", [playerId, newAngle])
     }
   }
 }
